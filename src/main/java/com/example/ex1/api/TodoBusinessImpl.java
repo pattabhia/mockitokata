daa7b1f20a563d5c0891fd1558311830f9fc7912
pattabhia <shivaram2004@gmail.com>
2019-08-26 23:37:31 +0530

    adding the SUT and dependency classes

---
+++ src/main/java/com/example/ex1/api/TodoBusinessImpl.java
@@ -0,0 +1,24 @@
+package com.example.ex1.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoBusinessImpl {
+
+    private TodoService todoService;
+
+    public TodoBusinessImpl(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public List<String> retrieveTodosRelatedToSpring(String user) {
+        List<String> filteredTodos = new ArrayList<>();
+        List<String> todos = todoService.retrieveTodos(user);
+        for (String todo : todos) {
+            if (todo.contains("Spring")) {
+                filteredTodos.add(todo);
+            }
+        }
+        return filteredTodos;
+    }
+}
---
+++ src/main/java/com/example/ex1/api/TodoService.java
@@ -0,0 +1,9 @@
+package com.example.ex1.api;
+
+import java.util.List;
+
+public interface TodoService {
+
+    public List<String> retrieveTodos(String user);
+
+}
