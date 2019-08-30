91dfce861d6da7c547bd6cc6104e46c5db22c4ae
pattabhia <shivaram2004@gmail.com>
2019-08-30 22:04:55 +0530

    refactor
    refactor

--- src/main/java/com/example/ex1/api/TodoBusinessImpl.java
+++ src/main/java/com/example/ex1/api/TodoBusinessImpl.java
@@ -1,20 +1,15 @@
 package com.example.ex1.api;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class TodoBusinessImpl {
-
-
 
     private TodoService todoService;
 
     public TodoBusinessImpl(TodoService todoService) {
         this.todoService = todoService;
     }
-
-
 
     public List<String> retrieveTodosRelatedToSpring(String user) {
         List<String> filteredTodos = new ArrayList<>();
